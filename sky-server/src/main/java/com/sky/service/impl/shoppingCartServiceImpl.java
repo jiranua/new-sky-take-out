db24c800898d8593d1d4db8ea909c1eafeb9e8da
lbq <luobingquan@pansoft.com>
2026-01-30 15:50:37 +0800

    查看购物车业务代码开发

--- sky-server/src/main/java/com/sky/service/impl/shoppingCartServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/shoppingCartServiceImpl.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -72,6 +73,21 @@ public class shoppingCartServiceImpl implements ShoppingCartService {
             shoppingCart.setCreateTime(LocalDateTime.now());
             shoppingCartMapper.insert(shoppingCart);
         }
+    }
+
+    /**
+     * 查看购物车
+     * @return
+     */
+    public List<ShoppingCart> showShoppingCart() {
+        // 获取到当前微信用户的id
+        Long userId = BaseContext.getCurrentId();
+        ShoppingCart shoppingCart = ShoppingCart.builder()
+                .userId(userId)
+                .build();
+
+        List<ShoppingCart> list = shoppingCartMapper.list(shoppingCart);
+        return list;
     }
 
 }
