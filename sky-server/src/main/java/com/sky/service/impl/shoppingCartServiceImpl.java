b0bb7a2ebe930253b6fb94555fb2862f7c0b45fe
lbq <luobingquan@pansoft.com>
2026-01-30 16:20:34 +0800

    清空购物车业务代码开发

--- sky-server/src/main/java/com/sky/service/impl/shoppingCartServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/shoppingCartServiceImpl.java
@@ -90,4 +90,13 @@ public class shoppingCartServiceImpl implements ShoppingCartService {
         return list;
     }
 
+    /**
+     * 清空购物车
+     */
+    public void cleanShoppingCart() {
+        // 获取到当前微信用户的id
+        Long userId = BaseContext.getCurrentId();
+        shoppingCartMapper.deleteByUserId(userId);
+    }
+
 }
