aed74c2acbce70a56db13058070c416f6c1a4784
lbq <luobingquan@pansoft.com>
2026-02-04 14:21:34 +0800

    取消订单功能代码开发

--- sky-server/src/main/java/com/sky/controller/user/OrderController.java
+++ sky-server/src/main/java/com/sky/controller/user/OrderController.java
@@ -78,9 +78,21 @@ public class OrderController {
      */
     @GetMapping("/orderDetail/{id}")
     @ApiOperation("查询订单详情")
-    public Result<OrderVO> getOrderDetail(@PathVariable Long id) {
+    public Result<OrderVO> getOrderDetail(@PathVariable("id") Long id) {
         OrderVO orderVO = orderService.getOrderDetail(id);
         return Result.success(orderVO);
+    }
+
+    /**
+     * 用户取消订单
+     *
+     * @return
+     */
+    @PutMapping("/cancel/{id}")
+    @ApiOperation("取消订单")
+    public Result cancel(@PathVariable("id") Long id) throws Exception {
+        orderService.userCancelById(id);
+        return Result.success();
     }
 
 }
